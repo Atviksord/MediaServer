7dd31c81fc122734f0ebd921f29b751be95ea193
Atviksord <Atviksord@gmail.com>
2024-10-02 23:39:05 +0900

    generation completed from DB

--- Fileserver.go
+++ Fileserver.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"strings"
 
 	"github.com/Atviksord/MediaServer/internal/database"
 )
@@ -33,13 +34,14 @@ func (cfg *apiconfig) templateInjector(w http.ResponseWriter, r *http.Request, u
 		return
 	}
 	// SQL get media from DB (NOT FULLY TESTED)
-	_, err = cfg.sqlMediaGetter(user)
+	datag, err := cfg.sqlMediaGetter(user)
+	fmt.Println(datag)
 	if err != nil {
 		fmt.Printf("Error getting Media data from SQL db %v", err)
 	}
 	// Data to inject will be generated from SQL database entries in the future.
 
-	datag := PageData{
+	/*datag := PageData{
 		User: userInfo{
 			Username: user.Username,
 		},
@@ -52,7 +54,7 @@ func (cfg *apiconfig) templateInjector(w http.ResponseWriter, r *http.Request, u
 			{Title: "Cool ducks running around", FilePath: "/static/Media/video1.mp4", Format: "video/mp4"},
 		},
 	}
-
+	*/
 	// Execute the template with the data
 	err = tmpl.Execute(w, datag)
 	if err != nil {
@@ -63,26 +65,29 @@ func (cfg *apiconfig) templateInjector(w http.ResponseWriter, r *http.Request, u
 }
 
 func (cfg *apiconfig) sqlMediaGetter(user database.User) (PageData, error) {
-
+	// Will make a custom page for favourited/followed videos.
 	allMedia, err := cfg.db.GetAllMedia(context.Background())
 	if err != nil {
 		fmt.Println("Couldnt get media data from database")
 	}
 	trueData := PageData{}
+	fmt.Println(allMedia[0].Format)
 
 	for _, datapoint := range allMedia {
 
 		trueData.User.Username = user.Username
 
-		if datapoint.Format == "video" {
+		if datapoint.MediaType == "video" {
 			trueData.Videos = append(trueData.Videos, MediaItem{Title: datapoint.MediaName, FilePath: datapoint.FilePath, Format: datapoint.Format})
 
 		}
-		if datapoint.Format == "image" {
-			trueData.Images = append(trueData.Images, MediaItem{Title: datapoint.MediaName, FilePath: datapoint.FilePath})
+		if datapoint.MediaType == "image" {
+			imagePath := strings.TrimPrefix(datapoint.FilePath, "static")
+			trueData.Images = append(trueData.Images, MediaItem{Title: datapoint.MediaName, FilePath: imagePath})
 
 		}
 
 	}
+	return trueData, nil
 
 }
