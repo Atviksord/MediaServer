ee5bf51f700a388346c415003b5ae4b256beacf0
Atviksord <Atviksord@gmail.com>
2024-10-25 22:54:51 +0900

    minor bug fix

--- favouriteHandler.go
+++ favouriteHandler.go
@@ -12,28 +12,26 @@ import (
 func (cfg *apiconfig) togglefavourite(w http.ResponseWriter, r *http.Request, user database.User) {
 	fmt.Println("GOES INTO TOGGLE MODE")
 	if r.Method == "POST" {
-		fmt.Println("LIKED")
-		StringMediaID := r.FormValue("mediaID")
 		favourite := r.FormValue("favourite")
-		fmt.Println(StringMediaID)
+		StringMediaID := r.FormValue("mediaID")
 		mediaID, err := strconv.Atoi(StringMediaID)
+
 		if err != nil {
 			fmt.Println("Failed to convert stringMediaID into MediaIT (integer)")
 		}
 		fmt.Println(favourite)
-
-		if favourite == "false" {
-			fmt.Println("Generating a new favourite")
-			_, err := cfg.db.AddFavourite(r.Context(), database.AddFavouriteParams{UserID: user.ID, MediaID: int32(mediaID)})
-			if err != nil {
-				fmt.Println("Unable to add favourite", err)
-			}
-
-		} else {
+		if favourite == "true" {
 			fmt.Println("Removing a favourite")
 			_, err := cfg.db.DeleteFavourite(r.Context(), database.DeleteFavouriteParams{UserID: user.ID, MediaID: int32(mediaID)})
 			if err != nil {
 				fmt.Println("Unable to remove favourite", err)
+			} else {
+				fmt.Println("Generating a new favourite")
+				_, err := cfg.db.AddFavourite(r.Context(), database.AddFavouriteParams{UserID: user.ID, MediaID: int32(mediaID)})
+				if err != nil {
+					fmt.Println("Unable to add favourite", err)
+				}
+
 			}
 		}
 		// RESERVE POINT
